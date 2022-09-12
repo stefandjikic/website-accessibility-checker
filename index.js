03ef03d16b5afd3c92b5dd4c74739358e1c7eff3
Stefan Djikic <stefandjikic@outlook.com>
2022-09-13 00:16:04 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const pa11y = require("pa11y");
+const PORT = process.env.PORT || 2225;
+
+const app = express();
+
+app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
+
+app.use(express.static('client'));
+
+app.get("/api/check", async (req, res) => {
+  if (!req.query.url) {
+    res.status(400).json({ error: "URL required" });
+  } else {
+    const data = await pa11y(req.query.url);
+    res.status(200).json(data);
+  }
+});
