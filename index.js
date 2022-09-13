b6d019489f720220362cedca509494b7e0a1cea6
Stefan Djikic <stefandjikic@outlook.com>
2022-09-14 00:12:07 +0200

    server code error handling

--- index.js
+++ index.js
@@ -6,13 +6,18 @@ const app = express();
 
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.get("/api/check", async (req, res) => {
   if (!req.query.url) {
     res.status(400).json({ error: "URL required" });
   } else {
-    const data = await pa11y(req.query.url);
-    res.status(200).json(data);
+    try {
+      const data = await pa11y(req.query.url);
+      res.status(200).json(data);
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
   }
 });
