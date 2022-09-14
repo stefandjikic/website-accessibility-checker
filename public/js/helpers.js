d6964d3e108207b7b0d69cf2a6463709258cf22e
Stefan Djikic <stefandjikic@outlook.com>
2022-09-14 23:12:27 +0200

    html rendering

---
+++ public/js/helpers.js
@@ -0,0 +1,8 @@
+const parseHTML = (html) => {
+  return html
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;')
+}
--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,6 @@
 const submit = document.getElementById("url-check");
-const submitBtn = document.getElementById('submit-btn');
+const submitBtn = document.getElementById("submit-btn");
+const resultsOutput = document.getElementById("results");
 
 submit.addEventListener("submit", async (e) => {
   e.preventDefault();
@@ -7,7 +8,7 @@ submit.addEventListener("submit", async (e) => {
   if (url === "") {
     alert("Please enter URL!");
   } else {
-    setLoader(true)
+    setLoader(true);
     const res = await fetch(`/api/check?url=${url}`);
 
     if (res.status !== 200) {
@@ -15,18 +16,39 @@ submit.addEventListener("submit", async (e) => {
       setLoader(false);
     } else {
       const data = await res.json();
-      console.log(data, "data");
+      // console.log(data, 'data')
+      renderResults(data.issues);
       setLoader(false);
     }
   }
 });
 
+const renderResults = (results) => {
+  resultsOutput.innerHTML = "";
+
+  if (results?.length === 0) {
+    resultsOutput.innerHTML =
+      "<h3>No problems found, everything looks fine. ✔️ </h3>";
+  } else {
+    results.forEach((res) => {
+      const output = `
+      <div class="card">
+        <h3 class="card-heading">${res.message}</h3>
+      <p class="code">${parseHTML(res.context)}</p>
+      <p class="code-sm">Selector: ${res.selector}</p>
+    </div>
+      `;
+      resultsOutput.innerHTML += output;
+    });
+  }
+};
+
 const setLoader = (isLoading = false) => {
   if (isLoading) {
     submitBtn.disabled = true;
-    submitBtn.innerText = 'Please wait...'
+    submitBtn.innerText = "Please wait...";
   } else {
     submitBtn.disabled = false;
-    submitBtn.innerText = 'Check'
+    submitBtn.innerText = "Check";
   }
-}
+};
